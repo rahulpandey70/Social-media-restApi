5c0d7cdfbe12934bb40e90f18f2e9b95e98469b2
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-05 10:04:53 +0530

    Verify hashed password

--- app/utils.py
+++ app/utils.py
@@ -2,5 +2,10 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+# Hashed Password
 def hashPassword(password: str):
     return pwd_context.hash(password)
+
+
+def verifyPassword(plainPass, hashedPass):
+    return pwd_context.verify(plainPass, hashedPass)
