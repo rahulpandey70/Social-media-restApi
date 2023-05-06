ee6d78b2df329b28a9bfa908fb1479ade8cff9b5
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-06 08:24:17 +0530

    Changes status code

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,19 +8,19 @@ from .. import models, schemas, utils, oauth2
 router = APIRouter(tags=['Authentication'])
 
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 def login(user_info: schemas.UserLogin, db: Session = Depends(get_db)):
 
     user = db.query(models.User).filter(
         models.User.email == user_info.email).first()
 
     if not user:
-        raise HTTPException(status_code=404, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=403, detail=f"Invalid Credentials")
 
     if not utils.verifyPassword(user_info.password, user.password):
-        raise HTTPException(status_code=404, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=403, detail=f"Invalid Credentials")
 
     # create token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"token": access_token, "type": "Bearer"}
+    return {"access_token": access_token, "token_type": "Bearer"}
