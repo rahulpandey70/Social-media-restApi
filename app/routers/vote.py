dd2ba48b4143cc19e467a547a3b28c6f5352ed3d
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-07 14:34:27 +0530

    Vote router

---
+++ app/routers/vote.py
@@ -0,0 +1,36 @@
+from fastapi import HTTPException, Depends, APIRouter, Response
+from sqlalchemy.orm import Session
+from .. import schemas, database, models, oauth2
+
+router = APIRouter(
+    prefix="/vote",
+    tags=["Vote"]
+)
+
+
+@router.post("/", status_code=201)
+def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    vote_query = db.query(models.Vote).filter(
+        models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
+    )
+
+    found_vote = vote_query.first()
+    if (vote.dir == 1):
+        if found_vote:
+            raise HTTPException(
+                status_code=409, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
+
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "Successfully voted"}
+
+    else:
+        if not found_vote:
+            raise HTTPException(status_code=404, detail="vote does not exist")
+
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+
+        return {"message": "successfully deleted vote"}
